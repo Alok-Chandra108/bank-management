aac6a4fcab1da007164a5ffbf0c7d0e9f0ae9087
Alok-Chandra108 <alokchandra2611@gmail.com>
2025-07-19 19:18:24 +0530

    Configure production build and serve frontend from Express

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ import cookieParser from 'cookie-parser';
 import compression from 'compression';
 
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+// Get __dirname equivalent in ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 dotenv.config();
 const app = express();
@@ -32,10 +37,10 @@ app.use('/api/bank', bankRoutes);
 app.use('/api/transactions', transactionRoutes);
 
 // ---------- Serve frontend in production ----------
-const __dirname = path.resolve();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/client/dist')));
 
+  // Handles all client-side routes
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'dist', 'index.html'));
   });
